503683ec7c4fbb1bf7c402487e37293074df1d3e
Desh Iyer <desh.iyer@gmail.com>
2023-11-25 00:43:46 +0530

    Wrap logic in main function

--- src/scripts/summarizeCode.py
+++ src/scripts/summarizeCode.py
@@ -17,28 +17,32 @@ def pythonTokenizer(line):
 
     return ' '.join(result)
 
-pipeline = SummarizationPipeline(
-    model=AutoModelWithLMHead.from_pretrained("SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune"),
-    tokenizer=AutoTokenizer.from_pretrained("SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune", skip_special_tokens=True),
-    device=0
-)
+def main():
+    pipeline = SummarizationPipeline(
+        model=AutoModelWithLMHead.from_pretrained("SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune"),
+        tokenizer=AutoTokenizer.from_pretrained("SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune", skip_special_tokens=True),
+        device=0
+    )
 
-code = '''
-if number <= 1:
-        return False
-    elif number <= 3:
-        return True
-    elif number % 2 == 0 or number % 3 == 0:
-        return False
-    i = 5
-    while i * i <= number:
-        if number % i == 0 or number % (i + 2) == 0:
+    code = '''
+    if number <= 1:
             return False
-        i += 6
-    return True
-'''
+        elif number <= 3:
+            return True
+        elif number % 2 == 0 or number % 3 == 0:
+            return False
+        i = 5
+        while i * i <= number:
+            if number % i == 0 or number % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+    '''
 
-tokenized_code = pythonTokenizer(code)
-print("Code after tokenization: " + tokenized_code)
+    tokenized_code = pythonTokenizer(code)
+    print("Code after tokenization: " + tokenized_code)
 
-pipeline([tokenized_code])
+    pipeline([tokenized_code])
+
+if __name__ == '__main__':
+    main()
