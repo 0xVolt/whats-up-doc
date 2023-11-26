f975b5b3a4aa87fd7510991fe9ad53f858ab84ed
Desh Iyer <desh.iyer@gmail.com>
2023-11-27 04:17:05 +0530

    Remove test code for model inference

--- src/main.py
+++ src/main.py
@@ -34,24 +34,6 @@ def main():
     code = readFile(r"C:\Users\deshi\Code\whats-up-doc\src\scripts\testReadFile.py")
     
     print(f"Code:\n\n{code}")
-    
-    # code = '''
-
-    # def is_prime(number):
-    #     if number <= 1:
-    #         return False
-    #     elif number <= 3:
-    #         return True
-    #     elif number % 2 == 0 or number % 3 == 0:
-    #         return False
-    #     i = 5
-    #     while i * i <= number:
-    #         if number % i == 0 or number % (i + 2) == 0:
-    #             return False
-    #         i += 6
-    #     return True
-            
-    # ''' #@param {type:"raw"}
 
     tokenizedCode = pythonTokenizer(code)
     print(f"\n\nCode after tokenization:\n\n{tokenizedCode}")
