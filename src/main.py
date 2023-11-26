f3e844f5063e0381dbdc6404911cb5d38c4f4062
Desh Iyer <desh.iyer@gmail.com>
2023-11-27 04:08:22 +0530

    Add function to read a script file

---
+++ src/main.py
@@ -0,0 +1,61 @@
+from transformers import AutoTokenizer, AutoModelWithLMHead, SummarizationPipeline
+import tokenize
+import io
+
+def pythonTokenizer(line):
+    result = []
+    line = io.StringIO(line)
+
+    for tokenType, token, start, end, line in tokenize.generate_tokens(line.readline):
+        if (not tokenType == tokenize.COMMENT):
+            if tokenType == tokenize.STRING:
+                result.append("CODE_STRING")
+            elif tokenType == tokenize.NUMBER:
+                result.append("CODE_INTEGER")
+            elif (not token == "\n") and (not token == "    "):
+                result.append(str(token))
+
+    return ' '.join(result)
+
+def readFile(path):
+    with open(path) as fin:
+        lines = fin.readlines()
+    
+    print(lines)
+    
+    # return code
+
+def main():
+    readFile(r"C:\Users\deshi\Code\whats-up-doc\src\scripts\testReadFile.py")
+    
+    # pipeline = SummarizationPipeline(
+    #     model=AutoModelWithLMHead.from_pretrained("SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune"),
+    #     tokenizer=AutoTokenizer.from_pretrained("SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune", skip_special_tokens=True),
+    #     device=0
+    # )
+
+    # code = '''
+
+    # def is_prime(number):
+    #     if number <= 1:
+    #         return False
+    #     elif number <= 3:
+    #         return True
+    #     elif number % 2 == 0 or number % 3 == 0:
+    #         return False
+    #     i = 5
+    #     while i * i <= number:
+    #         if number % i == 0 or number % (i + 2) == 0:
+    #             return False
+    #         i += 6
+    #     return True
+            
+    # ''' #@param {type:"raw"}
+
+    # tokenized_code = pythonTokenizer(code)
+    # # print("Code after tokenization: " + tokenized_code)
+
+    # print(pipeline([tokenized_code]))
+
+if __name__ == '__main__':
+    main()
