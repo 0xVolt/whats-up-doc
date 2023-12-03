073ce6cb27ae73ed3171378bffff20da421479ba
Desh Iyer <desh.iyer@gmail.com>
2023-12-04 04:42:43 +0530

    Create `__init__.py` to make a utils package

---
+++ src/utils/__init__.py
@@ -0,0 +1,17 @@
+import io
+import tokenize
+
+def pythonTokenizer(line):
+    result = []
+    line = io.StringIO(line)
+
+    for tokenType, token, start, end, line in tokenize.generate_tokens(line.readline):
+        if (not tokenType == tokenize.COMMENT):
+            if tokenType == tokenize.STRING:
+                result.append("CODE_STRING")
+            elif tokenType == tokenize.NUMBER:
+                result.append("CODE_INTEGER")
+            elif (not token == "\n") and (not token == "    "):
+                result.append(str(token))
+
+    return ' '.join(result)
