af56e98c8c76dd491c72c47d06e767125a6ad521
Desh Iyer <desh.iyer@gmail.com>
2023-12-04 09:30:39 +0530

    Add docstrings to util functions

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,11 +1,23 @@
 def readFile(path):
-    with open(path) as fin:
+    '''
+    Open a specified script file in read mode and return it's contents as a string.
+    
+    Argument(s):
+    path (string) - path to the file to be read
+    
+    Output(s):
+    code (string) - contents of the script file
+    '''
+    with open(path, 'r') as fin:
         code = fin.read()
     
     return code
 
 
 def checkGPU(tensorflow):
+    '''
+    Check if a GPU is recognized by Tensorflow or PyTorch 
+    '''
     if tensorflow == True:
         import tensorflow as tf
         print("Number of GPUs available with tensorflow:", len(tf.config.list_physical_devices('GPU')))
@@ -15,6 +27,15 @@ def checkGPU(tensorflow):
 
 
 def pythonTokenizer(line):
+    '''
+    Tokenize a python script file and replace code with abstracted syntax to then pass to a model inference point.
+    
+    Argument(s):
+    line (string) - Python code passed to tokenize
+    
+    Output(s):
+    result (string) - Result of the tokenized Python code
+    '''
     import io
     import tokenize
     
@@ -30,4 +51,6 @@ def pythonTokenizer(line):
             elif (not token == "\n") and (not token == "    "):
                 result.append(str(token))
 
-    return ' '.join(result)
+    result = ' '.join(result)
+
+    return result
