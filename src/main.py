10800449e27f58c5388f852dcc78b8f4dd91536a
Desh Iyer <desh.iyer@gmail.com>
2023-12-04 07:28:18 +0530

    Code \generate()\ command

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, SummarizationPipeline
-from utils import *
+from utils import readFile, pythonTokenizer
 import typer
 import tokenize
 import io
@@ -8,9 +8,32 @@ app = typer.Typer()
 
 
 @app.command()
-def generate(file: str, default: bool = True):
+def generate(file: str, default: bool = True, gpu: bool = True):
     print(f"File: {file}")
     print(f"Use Default Model: {default}")
+    
+    # Change gpu flag
+    device = 0 if gpu else 1
+
+    checkpoint = r"SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune" if default else -1
+    
+    pipeline = SummarizationPipeline(
+        model=AutoModelForSeq2SeqLM.from_pretrained(checkpoint),
+        tokenizer=AutoTokenizer.from_pretrained(
+            checkpoint,
+            skip_special_tokens=True,
+            legacy=False
+        ),
+        max_new_tokens=1024,
+        device=device
+    )
+
+    code = readFile(file)
+    tokenizedCode = pythonTokenizer(code)
+
+    print(f"Code:\n\n{code}")
+    print(f"\n\nCode after tokenization:\n\n{tokenizedCode}")
+    print(f"\n\nModel Output through inference point:\n\n{pipeline([tokenizedCode])}")
 
 
 @app.command()
@@ -20,23 +43,3 @@ def easter():
 
 if __name__ == "__main__":
     app()
-
-
-# checkpoint = r"SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune"
-#     pipeline = SummarizationPipeline(
-#         model=AutoModelForSeq2SeqLM.from_pretrained(checkpoint),
-#         tokenizer=AutoTokenizer.from_pretrained(
-#             checkpoint,
-#             skip_special_tokens=True,
-#             legacy=False
-#         ),
-#         max_new_tokens=1024,
-#         device=device
-#     )
-
-#     code = readFile(filePath)
-#     tokenizedCode = pythonTokenizer(code)
-
-#     print(f"Code:\n\n{code}")
-#     print(f"\n\nCode after tokenization:\n\n{tokenizedCode}")
-#     print(f"\n\nModel Output through inference point:\n\n{pipeline([tokenizedCode])}")
