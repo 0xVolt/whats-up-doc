dd8a4ce940fc78735744a26d1e15d40804fbfb58
Desh Iyer <desh.iyer@gmail.com>
2023-12-04 06:18:25 +0530

    Change app to single command template

--- src/main.py
+++ src/main.py
@@ -6,24 +6,24 @@ import io
 
 app = typer.Typer()
 
-@app.command
-def generate(filePath: str, defaultModel: bool, gpu: bool):
+
+def main(filePath: str):
     device = 0
-    if not gpu:
-        device = 1
+    # if not gpu:
+        # device = 1
     
-    if defaultModel:
-        checkpoint = r"SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune"
-        pipeline = SummarizationPipeline(
-            model=AutoModelForSeq2SeqLM.from_pretrained(checkpoint),
-            tokenizer=AutoTokenizer.from_pretrained(
-                checkpoint, 
-                skip_special_tokens=True, 
-                legacy=False
-            ),
-            max_new_tokens=1024,
-            device=device
-        )
+    # if defaultModel:
+    checkpoint = r"SEBIS/code_trans_t5_base_source_code_summarization_python_transfer_learning_finetune"
+    pipeline = SummarizationPipeline(
+        model=AutoModelForSeq2SeqLM.from_pretrained(checkpoint),
+        tokenizer=AutoTokenizer.from_pretrained(
+            checkpoint, 
+            skip_special_tokens=True, 
+            legacy=False
+        ),
+        max_new_tokens=1024,
+        device=device
+    )
     
     code = readFile(filePath)
     tokenizedCode = pythonTokenizer(code)
@@ -32,5 +32,6 @@ def generate(filePath: str, defaultModel: bool, gpu: bool):
     print(f"\n\nCode after tokenization:\n\n{tokenizedCode}")
     print(f"\n\nModel Output through inference point:\n\n{pipeline([tokenizedCode])}")
 
+
 if __name__ == '__main__':
-    app()
+    typer.run(main)
