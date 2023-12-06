3879a285751e8bd80bdfe8b8e6913782949b475b
Desh Iyer <desh.iyer@gmail.com>
2023-12-06 09:22:22 +0530

    Add test multiple expression parser

---
+++ src/experimental/scripts/ast_multi_parser.py
@@ -0,0 +1,40 @@
+import ast
+import os
+
+
+# Note: Python match-case statements are only supported in 3.10+
+def parseNode(node, path):
+    if isinstance(node, ast.Module):
+        return parseModule(node, path)
+    elif isinstance(node, ast.Expression):
+        return parseModule(node, path)
+    elif isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)):
+        return parseFunctionOrClass(node, path)
+    elif isinstance(node, ast.If):
+        return parseIf(node, path)
+    elif isinstance(node, ast.While):
+        return parseWhile(node, path)
+
+
+def parseModule():
+    pass
+
+
+def parseExpression():
+    pass
+
+
+def parseFunctionOrClass():
+    pass
+
+
+def parseIf():
+    pass
+
+
+def parseFor():
+    pass
+
+
+def parseWhile():
+    pass
