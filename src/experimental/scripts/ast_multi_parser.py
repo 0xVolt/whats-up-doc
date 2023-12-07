ad2f8bdf85dcb41c0f1f2d9f2d945f5855b46e8d
Desh Iyer <desh.iyer@gmail.com>
2023-12-07 12:13:25 +0530

    Update pretty printer

--- src/experimental/scripts/ast_multi_parser.py
+++ src/experimental/scripts/ast_multi_parser.py
@@ -3,12 +3,14 @@ import os
 
 
 def pretty(d, indent=0):
-   for key, value in d.items():
-      print('\t' * indent + str(key))
-      if isinstance(value, dict):
-         pretty(value, indent+1)
-      else:
-         print('\t' * (indent+1) + str(value))
+    for key, value in d.items():
+        print('\t' * indent + str(key))
+        
+        if isinstance(value, dict):
+            pretty(value, indent + 1)
+            
+        else:
+            print('\t' * (indent + 1) + str(value))
 
 
 def testScriptParsing(path):
