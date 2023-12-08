b7d3fb7b071f0446cee2f261d70b4e4e4f2f7908
Desh Iyer <desh.iyer@gmail.com>
2023-12-08 16:00:23 +0530

    Create a new module within the package

---
+++ src/utils/.py
@@ -0,0 +1,13 @@
+def checkGPU(tensorflow):
+    '''
+    Check if a GPU is recognized by Tensorflow or PyTorch
+    '''
+    if tensorflow == True:
+        import tensorflow as tf
+
+        print('Number of GPUs available with tensorflow', len(tf.config.list_physical_devices('GPU')))
+
+    else:
+        import torch
+
+        print('Checking if the GPU is available with PyTorch:', torch.cuda.is_available())
