0d0c8235fba8d2f2c16a435f9f4e9fe8332b92ed
Desh Iyer <desh.iyer@gmail.com>
2023-12-11 18:06:12 +0530

    Create a notebook to run inference on local GPT4All models

--- test/scripts/langchain/langchain_gpt4all.py
+++ test/scripts/langchain/langchain_gpt4all.py
@@ -1,6 +1,9 @@
-from langchain.llms import gpt4all
+from langchain.llms import GPT4All
 
-model = gpt4all(model=r"C:\Users\deshi\Code\gpt4all-models\orca-mini-3b-gguf2-q4_0.gguf")
+model = GPT4All(
+            model=r"C:\Users\deshi\Code\gpt4all-models\orca-mini-3b-gguf2-q4_0.gguf",
+            n_threads=8
+        )
 
 with open(r"C:\Users\deshi\Code\whats-up-doc\assets\test_prompt.txt", "r") as file:
     prompt = file.read()
