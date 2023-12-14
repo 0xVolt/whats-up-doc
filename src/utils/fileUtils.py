affc887ba6f4409817a85d3c254e7dbe9c5d8692
Desh Iyer <desh.iyer@gmail.com>
2023-12-14 19:00:17 +0530

    Rewrite function

--- src/utils/fileUtils.py
+++ src/utils/fileUtils.py
@@ -23,27 +23,5 @@ def returnModelLocalPath(model):
     return modelPath[model]
 
 
-def writeOutputToMarkdownFile(filename, strings, title=None, ordered=False):
-    """
-    Writes a list of strings to a markdown file with appropriate formatting.
-
-    Args:
-        filename: The path to the markdown file to write to.
-        strings: A list of strings to write to the file.
-        title (optional): The title for the markdown file (default: None).
-        ordered (optional): Whether to format the list as ordered (numbered) or unordered (bullet points) (default: False).
-
-    Returns:
-        None
-    """
-    import mdutils
+def writeOutputToMarkdownFile(fileName, outputList, title="Document", ordered=False):
     
-    mdFile = mdutils.MdUtils(filename, title=title)
-
-    # Choose formatting based on ordered flag
-    if ordered:
-        mdFile.new_list(strings, marked_with='-')
-    else:
-        mdFile.new_list(strings)
-
-    mdFile.create_md_file()
