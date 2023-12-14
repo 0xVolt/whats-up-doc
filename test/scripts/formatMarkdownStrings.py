4c649d4de1bec67cfbd1cfe290b47d434a99ad24
Desh Iyer <desh.iyer@gmail.com>
2023-12-14 19:00:52 +0530

    Update test function script

--- test/scripts/formatMarkdownStrings.py
+++ test/scripts/formatMarkdownStrings.py
@@ -1,28 +1,49 @@
-import markdown2
+import textwrap
 
-text = """
-Function Name: log_directory_structure
+def format_function_description(input_string):
+    # Split the input string into lines
+    lines = input_string.strip().split('\n')
 
+    # Extract function name and description
+    function_name = lines[0].split(":")[1].strip()
+    description = lines[1].split(":")[1].strip()
+
+    # Extract arguments, return values, and explanation
+    arguments_start = lines.index("Arguments:") + 1
+    return_values_start = lines.index("Return Values:") + 1
+    explanation_start = lines.index("Explanation:") + 1
+
+    arguments = '\n'.join(lines[arguments_start:return_values_start-1])
+    return_values = '\n'.join(lines[return_values_start:explanation_start-1])
+    explanation = '\n'.join(lines[explanation_start:])
+
+    # Format the output
+    formatted_output = f"## Function Name: `{function_name}`\n\n{description}\n\n" \
+                       f"### Description\n\n{textwrap.indent(description, '* ')}\n\n" \
+                       f"### Arguments\n\n{textwrap.indent(arguments.replace('- ', '* '), '')}\n\n" \
+                       f"### Return Values\n\n{return_values}\n\n" \
+                       f"### Explanation\n{textwrap.indent(explanation, '')}"
+
+    return formatted_output
+
+# Example usage
+input_string = """
+Function Name: functionxyz
 Description: This function logs the directory structure by recursively traversing through all subdirectories and files in the given directory path.
-
 Arguments:
-- `directory_path` (str): The path of the root directory to be logged. It should exist on the file system.
-- `ai_context` (dict): A dictionary containing any additional context information that needs to be passed along with the log messages.
-- `indent` (int, optional): An integer specifying how many spaces to indent each level of subdirectories in the output. Default is 0.
-
+- directory_path (str): The path of the root directory to be logged. It should exist on the file system.
+- ai_context (dict, optional): A dictionary containing any additional context information that needs to be passed along with the log messages.
+- indent (int, optional): An integer specifying how many spaces to indent each level of subdirectories in the output. Default is 0.
 Return Values:
 None - This function does not return a value.
-
 Explanation:
-1. The function first checks if the given directory path exists on the file system using `os.path.exists()` and prints an error message if it doesn't exist.
-2. It then gets a list of all items in the directory using `os.listdir()`.
-3. For each item, it constructs the full path to that item by joining the directory path with the item name using `os.path.join()` and checks if it's a directory or file using `os.path.isdir()`.
-4. If the item is a directory, it logs the directory using the given `ai_context` dictionary and recursively calls itself on that subdirectory with an increased indentation level using `log_directory_structure(item_path, ai_context, indent + 1)`.
-5. If the item is a file, it can log it similarly.
-6. The function then prompts the user to enter the directory path and logs the root directory with that path.
-7. Finally, it calls itself on the given `directory_path` using `log_directory_structure(directory_path, ai_context)`.
+1. The function first checks if the given directory path exists using os.path.exists() and prints an error message if it doesn't exist.
 """
 
-html = markdown2.markdown(text)
+formatted_output = format_function_description(input_string)
 
-print(html)
+# Write the formatted string to a markdown file
+with open('formatted_function.md', 'w') as file:
+    file.write(formatted_output)
+
+print("Formatted string has been written to 'formatted_function.md'")
