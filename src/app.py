0fe69df13e148f41da15ddb88ede21193f7afd93
Desh Iyer <desh.iyer@gmail.com>
2023-12-14 21:41:52 +0530

    Remove test code from app

--- src/app.py
+++ src/app.py
@@ -41,33 +41,8 @@ def generate(
 
     llmChain = modelUtils.setupLangChain(model)
 
-    # for function in functions:
-    #     modelOutputs.append(llmChain.run({'function': function}))
-
-    testOutput = """
-Function Name: log_directory_structure
-
-Description: This function logs the directory structure by recursively traversing through all subdirectories and files in the given directory path.
-
-Arguments:
-- `directory_path` (str): The path of the root directory to be logged. It should exist on the file system.
-- `ai_context` (dict): A dictionary containing any additional context information that needs to be passed along with the log messages.
-- `indent` (int, optional): An integer specifying how many spaces to indent each level of subdirectories in the output. Default is 0.
-
-Return Values:
-None - This function does not return a value.
-
-Explanation:
-1. The function first checks if the given directory path exists on the file system using `os.path.exists()` and prints an error message if it doesn't exist.
-2. It then gets a list of all items in the directory using `os.listdir()`.
-3. For each item, it constructs the full path to that item by joining the directory path with the item name using `os.path.join()` and checks if it's a directory or file using `os.path.isdir()`.
-4. If the item is a directory, it logs the directory using the given `ai_context` dictionary and recursively calls itself on that subdirectory with an increased indentation level using `log_directory_structure(item_path, ai_context, indent + 1)`.
-5. If the item is a file, it can log it similarly.
-6. The function then prompts the user to enter the directory path and logs the root directory with that path.
-7. Finally, it calls itself on the given `directory_path` using `log_directory_structure(directory_path, ai_context)`. 
-"""
-
-    modelOutputs.append(testOutput)
+    for function in functions:
+        modelOutputs.append(llmChain.run({'function': function}))
 
     fileUtils.writeOutputToMarkdownFile(outputFile, modelOutputs, title="Test Output Markdown File")
 
