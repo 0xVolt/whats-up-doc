32cc1c31087cec76ff7dcb08e42afec760614a4f
Desh Iyer <desh.iyer@gmail.com>
2024-01-30 18:55:41 +0530

    Update test script of functions

--- src/test.py
+++ src/test.py
@@ -1,10 +1,10 @@
-# def checkGPU(tensorflow):
-#     if tensorflow == True:
-#         import tensorflow as tf
-#         print("Number of GPUs available with tensorflow:", len(tf.config.list_physical_devices('GPU')))
-#     else:
-#         import torch
-#         print('Checking if the GPU is available with PyTorch:', torch.cuda.is_available())
+def checkGPU(tensorflow):
+    if tensorflow == True:
+        import tensorflow as tf
+        print("Number of GPUs available with tensorflow:", len(tf.config.list_physical_devices('GPU')))
+    else:
+        import torch
+        print('Checking if the GPU is available with PyTorch:', torch.cuda.is_available())
 
 def sum():
     sum = 0
