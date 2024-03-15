ca4d000c946c3a62b383aa09a813d8359485ae46
Desh Iyer <desh.iyer@gmail.com>
2024-03-15 18:14:12 +0530

    Update: Test file with sample function(s)

--- src/test.py
+++ src/test.py
@@ -1,11 +1,3 @@
-def checkGPU(tensorflow):
-    if tensorflow == True:
-        import tensorflow as tf
-        print("Number of GPUs available with tensorflow:", len(tf.config.list_physical_devices('GPU')))
-    else:
-        import torch
-        print('Checking if the GPU is available with PyTorch:', torch.cuda.is_available())
-
 def sum():
     sum = 0
     for i in range(10):
