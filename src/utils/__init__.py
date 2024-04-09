f0875c5e4e77da6938198b92ececc6f978cfe30c
Desh Iyer <desh.iyer@gmail.com>
2024-04-09 13:20:49 +0530

    Update: Remove dead code from utils package

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,2 @@
-# Specify names of the modules within the utils package in this list.
+# Specify names of the modules within the utils package in this list
 __all__ = ['fileUtils', 'generalUtils', 'parserUtils', 'modelUtils']
--- src/utils/fileUtils.py
+++ src/utils/fileUtils.py
@@ -1,38 +1,5 @@
-from .parserUtils import formatModelOutputToMarkdown
-
-
-def readFile(path):
-    '''
-    Open a specified script file in read mode and return it's contents as a string.
-
-    Argument(s):
-    path (string) - path to the file to be read
-
-    Output(s):
-    code (string) - contents of the script file
-    '''
-    with open(path, 'r') as fin:
-        code = fin.read()
-
-    return code
-
-
-# def returnModelLocalPath(model):
-#     modelPath = {
-#         'mistral-7b-instruct': r"C:\Users\deshi\Code\models\mistral-7b-instruct-v0.1.Q4_0.gguf",
-#         'orca-mini-3b': r"C:\Users\deshi\Code\models\orca-mini-3b-gguf2-q4_0.gguf"
-#     }
-
-#     return modelPath[model]
-
-
 def writeOutputToMarkdownFile(fileName, outputList, title):
-    # formattedOutputs = []
-
-    # for output in outputList:
-    #     formattedOutputs.append(formatModelOutputToMarkdown(output))
-
-    # # Join the formatted outputs into a single string
+    # Join the formatted outputs into a single string
     markdownContent = f"# {title}\n\n" + "\n\n---\n\n".join(outputList)
 
     # Write the content to the file
