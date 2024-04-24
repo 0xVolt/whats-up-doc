f4366a9b4b927389cda1321e36cf2b485ce2e741
Desh Iyer <desh.iyer@gmail.com>
2024-04-24 21:19:42 +0530

    Create script to determine the language a script file is in

---
+++ test-code/scripts/guesslang.py
@@ -0,0 +1,14 @@
+# A script file to determine the programming language of a script file by checking it against a dictionary of file extensions
+
+fileExtensionMap = = {
+    ".py": "Python",
+    ".js": "JavaScript",
+    ".java": "Java",
+    ".cpp": "C++",
+    ".c": "C",
+    ".html": "HTML",
+    ".css": "CSS",
+    ".php": "PHP",
+    ".sh": "Bash"
+}
+
