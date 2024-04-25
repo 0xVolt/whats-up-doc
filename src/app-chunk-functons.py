887a40a4e3577ee0dacc3466e9ec8063c2e0ab10
Desh Iyer <desh.iyer@gmail.com>
2024-04-26 00:52:34 +0530

    Create a copy of the current state of parsed Python functions being passed to the LangChain

---
+++ src/app-chunk-functons.py
@@ -0,0 +1,55 @@
+import typer
+# from yaspin import yaspin
+from utils import *
+
+app = typer.Typer()
+# spinner = yaspin()
+
+@app.command()
+def generate_script_documentation(
+    path: str,
+    model: str = typer.Argument(None),
+    outputFile: str = 'output'
+):
+    '''
+    Typer command to generate the documentation for an input script file.
+
+    Arguments:
+    filePath (string) - path to the input file
+    model (string) - string to specify which model to use when generating documentation
+    '''
+
+    if model is None:
+        model = modelUtils.getModelChoice()
+
+    print(f"Arguments specified:")
+    print(f"File Path: {path}")
+    print(f"Model: {model}\n")
+    
+    language = fileUtils.getScriptLanguage(path)
+    
+    # Parse script according to language
+    # functions = parserUtils.extractFunctionsAsList(path, language)
+
+    chain = modelUtils.setupLangChain(model)
+
+    print("Generating model outputs...")
+
+    modelOutputs = []
+
+    for function in functions:
+        stream = chain.stream({'function': function})
+        outputString = ''
+        
+        for chunk in stream:                
+            # Print each chunk to the terminal
+            print(chunk, end='')
+            # Concatenate each chunk to the output string
+            outputString += chunk
+        
+        modelOutputs.append(outputString)
+
+    fileUtils.writeOutputToMarkdownFile(outputFile, modelOutputs, title=f"Documentation for `{path}`")
+
+if __name__ == "__main__":
+    app()
