582999ead0eef6f4ce35e0fdc18de29ffc079dbb
Desh Iyer <desh.iyer@gmail.com>
2024-05-01 23:21:32 +0530

    Create `.js` file of test functions

---
+++ src/test_doc_scripts/test_functions.js
@@ -0,0 +1,26 @@
+// Function to generate a random password
+function generateRandomPassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+// Function to check if a number is prime
+function isPrime(number) {
+    if (number <= 1) return false;
+    if (number <= 3) return true;
+
+    if (number % 2 === 0 || number % 3 === 0) return false;
+
+    let i = 5;
+    while (i * i <= number) {
+        if (number % i === 0 || number % (i + 2) === 0) return false;
+        i += 6;
+    }
+
+    return true;
+}
