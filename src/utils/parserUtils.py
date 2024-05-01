0b2301b5c3a18e813507c2597ba62d4d21d0273d
Desh Iyer <desh.iyer@gmail.com>
2024-05-01 23:07:25 +0530

    Update multi-parser script with match-case logic

--- src/utils/parserUtils.py
+++ src/utils/parserUtils.py
@@ -3,7 +3,52 @@ import json
 import re
 import textwrap
 
-def extractFunctionsAsList(path, language):
+fileExtensionMap = {
+    "py": "Python",
+    "js": "JavaScript",
+    "java": "Java",
+    "cpp": "C++",
+    "c": "C",
+    "go": "Golang",
+    "html": "HTML",
+    "css": "CSS",
+    "php": "PHP",
+    "sh": "Bash",
+    "sc": "Scala",
+    "scala": "Scala",
+    "md": "Markdown"
+}
+
+def getFileExtension(file_path):
+    # Regular expression pattern to match file extension
+    pattern = r'\.(?!\.)[a-zA-Z0-9+]+$'
+
+    # Use regex expression to get file extension
+    match = re.search(pattern, file_path)
+
+    if match:
+        # Remove leading period
+        return match.group(0)[1:]
+    else:
+        return None
+
+def getScriptLanguage(path):
+
+    """
+    Determine the programming language based on the file extension.
+
+    Args:
+    - file_extension (str): The file extension of the script file.
+
+    Returns:
+    - str: The programming language corresponding to the file extension.
+    """
+    extension = getFileExtension(path)
+    language = fileExtensionMap.get(extension.lower(), "Unknown")
+
+    return language
+
+def extractFunctionsAsListPython(path):
     listOfFunctions = []
 
     with open(path, 'r') as file:
@@ -28,3 +73,11 @@ def extractFunctionsAsList(path, language):
     listOfFunctionBodies = [function['body'] for function in listOfFunctions]
 
     return listOfFunctionBodies
+
+def extractFunctionsAsList(path, language):
+    match language:
+        case "Python":
+            return extractFunctionsAsListPython(path)
+
+        case _:
+            print("Unsupported language...")
